a880d38905e3e8eb3a0f5f754986cc6951d23a24
Jose Mariano Soto Núñez <31678416+Marianito412@users.noreply.github.com>
2026-03-22 14:45:27 -0600

    Fixed stuff

--- src/pages/Home.pageTest.tsx
+++ src/pages/Home.pageTest.tsx
@@ -1,5 +1,3 @@
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Welcome } from '../components/Welcome/Welcome';
 import { NavbarMinimalColored} from '../components/NavBar/NavBar';
 
 export function HomePageTest() {
